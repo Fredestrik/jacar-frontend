13a71d46e2af8de512de0ea0d55a2b75ff6342d2
Frédéric Lang <f.lang@hotmail.fr>
2020-07-28 20:15:48 +0200

    small pictures in trip page

--- src/pages/trips/[id].js
+++ src/pages/trips/[id].js
@@ -1,6 +1,25 @@
 import { useRouter } from 'next/router'
 
-export default ({trip}) => (
+export default ({trip}) => {
+  const router = useRouter()
+    // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return (
+      <main>
+        <section className="hero">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Loading..
+                </h1>
+              </div>
+            </div>
+          </section>
+        </main>
+    )
+  }
+  return (
     <main>
       <section className="hero">
           <div className="hero-body">
@@ -19,13 +38,8 @@ export default ({trip}) => (
         <div className="tile is-ancestor">
           <div className="tile is-parent is-vertical">
           { trip.pictures.map(picture => (
-            <div className="tile is-child is-12 box">
-              <p className="title">{picture.name}</p>
-              <p className="subtitle">url : {picture.url}</p>
-              <p className="subtitle">size : {picture.size}</p>
-              <p className="subtitle">hash : {picture.hash}</p>
-              <p className="subtitle">mime : {picture.mime}</p>
-              <img src={picture.url} alt="Jacar Photo" ></img>
+            <div className="tile is-child is-4">
+              <img src={picture.formats.small.url} alt="Jacar Photo" ></img>
             </div>
           ))}
           </div>
@@ -34,14 +48,25 @@ export default ({trip}) => (
         </section>
     </main>
   )
+}
 
-export async function getServerSideProps(context) {
-    const { id } = context.query
-    const res = await fetch("https://jacar-strapi.herokuapp.com/trips/"+id)
-    const trip = await res.json()
-    return {
-      props : {
-        trip,
-      }
-    }
+// This function gets called at build time
+export async function getStaticPaths() {
+  return {
+    // Only `/posts/1` and `/posts/2` are generated at build time
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    // Enable statically generating additional pages
+    // For example: `/posts/3`
+    fallback: true,
   }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  const url = `${process.env.API_URL}/trips/${params.id}`
+  const res = await fetch(url)
+  const trip = await res.json()
+  // Pass post data to the page via props
+  return { props: {trip } }
+}
