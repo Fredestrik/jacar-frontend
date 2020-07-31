782305ee206bd9aa69f84ce4b574e69f90f9ec95
Frédéric Lang <f.lang@hotmail.fr>
2020-07-31 17:33:34 +0200

    rename pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+
+
+export default () => (
+  <div className="container">
+    <main align="center">
+      <h1 className="title">
+        Welcome to Jacar!
+      </h1>
+
+      <img src="/jacar.png" alt="jacar picture" width="50%" />
+    </main>
+  </div>
+)
+
